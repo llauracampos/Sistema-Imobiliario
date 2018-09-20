217f6093d6062e6bdf6e9dff750ab6df78dc49a9
llauracampos <41303661+llauracampos@users.noreply.github.com>
2018-09-20 14:03:23 -0300

    Add files via upload

---
+++ Projeto 2 atualizado/Projeto 2 atualizado/include/Casa.h
@@ -0,0 +1,33 @@
+#ifndef CASA_H
+#define CASA_H
+#include "Imoveis.h"
+#include <iostream>
+
+class Casa:public Imoveis
+{
+    public:
+        Casa();
+
+        void setNumPavimentos(int);
+        void setNumQuartos(int);
+        void setAreaTerreno(float);
+        void setAreaConstruida(float);
+
+        int getNumPavimentos();
+        int getNumQuartos();
+        float getAreaTerreno();
+        float getAreaConstruida();
+
+        friend ostream& operator <<(ostream& saida, Casa& novaCasa){
+            saida << novaCasa.areaConstruida << endl;
+            return saida;
+        }
+
+    private:
+        int numPavimentos;
+        int numQuartos;
+        float areaTerreno;
+        float areaConstruida;
+};
+
+#endif // CASA_H
